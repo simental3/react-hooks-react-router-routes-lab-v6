4b05895c59d52896d6d316903bee662c84447aed
Anthony Simental <anthonysimental12@gmail.com>
2023-11-07 10:41:41 -0800

    completed

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import Card from "../components/ReusableCard";
 
 function Actors() {
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/actors')
+      .then((r) => r.json())
+      .then((actorData) => setActors(actorData))
+  }, [])
+
+  const actorsList = actors.map(({name, movies}) => (
+    <Card key={name} name={name} movies={movies} />
+  ))
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actorsList}
       </main>
     </>
   );
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,20 @@
+import { useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.error(error)
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Oops! Looks like something went wrong!</h1>
+      </main>
+    </>
+  )
+}
+
+export default ErrorPage;
